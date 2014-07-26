4218f798909674c237dce2f198d57db08748dbd1
Ricky Elrod <ricky@elrod.me>
2014-07-26 07:04:24 -0400

    Fix indentation of TerminalLib
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- pureio-core/src/main/java/me/elrod/pureio/TerminalLib.java
+++ pureio-core/src/main/java/me/elrod/pureio/TerminalLib.java
@@ -4,15 +4,15 @@ import java.util.function.Function;
 
 /** This provides a nicer public API over the rest of the code. */
 public class TerminalLib {
-  public static PureIO<Unit> putStrLn(String s) {
-    return new TerminalOperation.PutStrLn<Unit>(s, Unit.VALUE).liftF();
-  }
+    public static PureIO<Unit> putStrLn(String s) {
+        return new TerminalOperation.PutStrLn<Unit>(s, Unit.VALUE).liftF();
+    }
 
-  public static PureIO<String> readLine() {
-    return new TerminalOperation.ReadLine<String>(Function.identity()).liftF();
-  }
+    public static PureIO<String> readLine() {
+        return new TerminalOperation.ReadLine<String>(Function.identity()).liftF();
+    }
 
-  public static PureIO<Unit> exit(Integer exitCode) {
-      return new TerminalOperation.Exit<Unit>(exitCode, Unit.VALUE).liftF();
-  }
+    public static PureIO<Unit> exit(Integer exitCode) {
+        return new TerminalOperation.Exit<Unit>(exitCode, Unit.VALUE).liftF();
+    }
 }
