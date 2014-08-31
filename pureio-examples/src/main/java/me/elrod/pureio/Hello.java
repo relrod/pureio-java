c395b6a20cfb0680126bc04ba5cf2f45a2e0fb4d
Ricky Elrod <ricky@elrod.me>
2014-08-30 23:10:54 -0400

    [examples] This is a much better use of bind.
    Rather than binding outside of the function each time, bind within it
    so that you can keep multiple variables in scope.
    
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- pureio-examples/src/main/java/me/elrod/pureio/Hello.java
+++ pureio-examples/src/main/java/me/elrod/pureio/Hello.java
@@ -2,14 +2,14 @@ import me.elrod.pureio.*;
 
 public class Hello {
     private static PureIO<Unit> program =
-      TerminalLib.putStrLn("What is your name?")
-      .flatMap(x -> TerminalLib.readLine())
-      .flatMap(x -> TerminalLib.putStrLn("Hi there, " + x + "! How are you?"))
-      .flatMap(x -> TerminalLib.readLine())
-      .flatMap(x -> TerminalLib.putStrLn("I am also " + x + "!"))
-      .flatMap(x -> TerminalLib.exit(0));
+        TerminalLib.putStrLn("What is your name?")
+        .flatMap(x -> TerminalLib.readLine()
+                 .flatMap(name -> TerminalLib.putStrLn("Hi there, " + name + "! How are you?")
+                          .flatMap(u1 -> TerminalLib.readLine()
+                                   .flatMap(resp -> TerminalLib.putStrLn("I am also " + resp + "!")
+                                            .flatMap(u2 -> TerminalLib.exit(0))))));
 
-  public static void main(String[] args) {
-      UnsafePerformIO.unsafePerformIO(program);
-  }
+    public static void main(String[] args) {
+        UnsafePerformIO.unsafePerformIO(program);
+    }
 }
--- pureio-examples/src/main/java/me/elrod/pureio/HelloTrampoline.java
+++ pureio-examples/src/main/java/me/elrod/pureio/HelloTrampoline.java
@@ -2,14 +2,14 @@ import me.elrod.pureio.*;
 
 public class HelloTrampoline {
     private static PureIOT<Unit> program =
-      TerminalLib.putStrLnT("What is your name?")
-      .flatMap(x -> TerminalLib.readLineT())
-      .flatMap(x -> TerminalLib.putStrLnT("Hi there, " + x + "! How are you?"))
-      .flatMap(x -> TerminalLib.readLineT())
-      .flatMap(x -> TerminalLib.putStrLnT("I am also " + x + "!"))
-      .flatMap(x -> TerminalLib.exitT(0));
+        TerminalLib.putStrLnT("What is your name?")
+        .flatMap(x -> TerminalLib.readLineT()
+                 .flatMap(name -> TerminalLib.putStrLnT("Hi there, " + name + "! How are you?")
+                          .flatMap(u1 -> TerminalLib.readLineT()
+                                   .flatMap(resp -> TerminalLib.putStrLnT("I am also " + resp + "!")
+                                            .flatMap(u2 -> TerminalLib.exitT(0))))));
 
-  public static void main(String[] args) {
-      program.run(x -> UnsafePerformIO.unsafePerformIOT(x));
-  }
+    public static void main(String[] args) {
+        program.run(x -> UnsafePerformIO.unsafePerformIOT(x));
+    }
 }
