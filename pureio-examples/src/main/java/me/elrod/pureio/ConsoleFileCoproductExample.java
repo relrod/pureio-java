557a03f3aa903318fc61034f0303b169f78e3ab1
Ricky Elrod <ricky@elrod.me>
2014-10-09 16:49:04 -0400

    line it up? [ci skip]
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- pureio-examples/src/main/java/me/elrod/pureio/ConsoleFileCoproductExample.java
+++ pureio-examples/src/main/java/me/elrod/pureio/ConsoleFileCoproductExample.java
@@ -5,9 +5,9 @@ public class ConsoleFileCoproductExample {
     public static void main(String[] args) {
         ConsoleFileFree<LinkedList<String>> first =
             ConsoleFileLib.putStrLn("What is your name?").liftF()
-            .$ (x -> ConsoleFileLib.readLine().liftF()
-            .$ (name -> ConsoleFileLib.putStrLn("Hi there, " + name + "! Give me a file to read:").liftF()
-            .$ (u1 -> ConsoleFileLib.readLine().liftF()
+            .$ (x     -> ConsoleFileLib.readLine().liftF()
+            .$ (name  -> ConsoleFileLib.putStrLn("Hi there, " + name + "! Give me a file to read:").liftF()
+            .$ (u1    -> ConsoleFileLib.readLine().liftF()
             .$ (fname -> ConsoleFileLib.readLines(fname).liftF()))));
 
         // This doesn't have to be separate, but why not?
