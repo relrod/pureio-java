ababf1699f618cd41f05d98655ba366350ba5832
Ricky Elrod <ricky@elrod.me>
2014-10-09 16:52:44 -0400

    combine the two programs
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- pureio-examples/src/main/java/me/elrod/pureio/ConsoleFileCoproductExample.java
+++ pureio-examples/src/main/java/me/elrod/pureio/ConsoleFileCoproductExample.java
@@ -3,20 +3,14 @@ import java.util.function.Function;
 
 public class ConsoleFileCoproductExample {
     public static void main(String[] args) {
-        ConsoleFileFree<LinkedList<String>> first =
+        ConsoleFileFree<LinkedList<Unit>> program =
             ConsoleFileLib.putStrLn("What is your name?").liftF()
             .$ (x     -> ConsoleFileLib.readLine().liftF()
             .$ (name  -> ConsoleFileLib.putStrLn("Hi there, " + name + "! Give me a file to read:").liftF()
             .$ (u1    -> ConsoleFileLib.readLine().liftF()
-            .$ (fname -> ConsoleFileLib.readLines(fname).liftF()))));
+            .$ (fname -> ConsoleFileLib.readLines(fname).liftF()))))
+            .$ (lines -> ConsoleFileFree.mapM(y -> ConsoleFileLib.putStrLn(y).liftF(), lines));
 
-        // This doesn't have to be separate, but why not?
-        ConsoleFileFree<LinkedList<Unit>> second =
-            first
-            .$ (x -> ConsoleFileFree.mapM(
-                    y -> ConsoleFileLib.putStrLn(y).liftF(),
-                    x));
-
-        UnsafePerformIO.unsafePerformConsoleFileIO(second);
+        UnsafePerformIO.unsafePerformConsoleFileIO(program);
     }
 }
