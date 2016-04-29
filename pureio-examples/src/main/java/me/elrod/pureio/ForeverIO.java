4cdc8dbb85ad628583497ac468d0a22567883c13
Ricky Elrod <ricky@elrod.me>
2016-04-29 01:58:11 -0400

    I lied
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- pureio-examples/src/main/java/me/elrod/pureio/ForeverIO.java
+++ pureio-examples/src/main/java/me/elrod/pureio/ForeverIO.java
@@ -3,7 +3,7 @@ import me.elrod.pureio.*;
 public class ForeverIO {
     private static PureConsoleIO<Unit> program =
         PureConsoleIO.forever(
-            TerminalLib.putStrLn("This will print for a bit then bomb out."));
+            TerminalLib.putStrLn("This will bomb out with SOE."));
 
   public static void main(String[] args) {
       UnsafePerformIO.unsafePerformIO(program);
